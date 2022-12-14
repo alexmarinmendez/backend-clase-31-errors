557626b003d452660d9494e53a531a253ad12e79
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-12-13 20:31:30 -0500

    feat: add validations custom messages

--- src/validations.js
+++ src/validations.js
@@ -2,7 +2,12 @@ const yup = require('yup')
 
 const createUserValidation = (data) => {
     const schema = yup.object().shape({
-        name: yup.string().min(5).matches(/^[a-z]+$/).required()
+        // name: yup.string().min(5).matches(/^[a-z]+$/).required()
+        name: yup
+            .string('El campo name debe ser un string')
+            .min(5, 'El campo name debe ser como mínimo de 5 caracteres de longitud')
+            .matches(/^[a-z]+$/, 'El campo name debe contener unicamente letras de la a-z')
+            .required('El campo name es obligatorio')
     })
     schema.validateSync(data)
 }
