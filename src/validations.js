ee022152484c6c7e393930f884e2296b8b1ddedc
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-12-13 20:27:50 -0500

    feat: add validation with yup

--- src/validations.js
+++ src/validations.js
@@ -1,23 +1,10 @@
-const createUserValidation = (data) => {
-    const { name, age, email } = data
-    
-    if (!name) {
-        // return res.status(400).send({ error: 'name is required' })
-        throw new Error('name is required')
-    }
-    if (typeof name !== 'string') {
-        // return res.status(400).send({ error: 'name must be a string' })
-        throw new Error('name must be a string')
-    }
-    if (name.length < 5) {
-        // return res.status(400).send({ error: 'name must be at least 5 characters long' })
-        throw new Error('name must be at least 5 characters long')
-    }
-    if (!/^[a-z]+$/i.test(name)) {
-        // return res.status(400).send({ error: 'name must contains only a-z characters' })
-        throw new Error('name must contains only a-z characters')
-    }
+const yup = require('yup')
 
+const createUserValidation = (data) => {
+    const schema = yup.object().shape({
+        name: yup.string().min(5).matches(/^[a-z]+$/).required()
+    })
+    schema.validateSync(data)
 }
 
 module.exports = {
